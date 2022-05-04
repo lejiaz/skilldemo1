67146eecd1125843b8668574bcf529e7904d9e2b
Lejia <lez002@ucsd.edu>
2022-05-03 17:01:23 -0700

    update

--- SkillDemo1.java
+++ SkillDemo1.java
@@ -1,5 +1,7 @@
 public class SkillDemo1{
-
-
+    
+    public static int subtract(int a, int b) {
+        return a-b;
+    }
 
 }
---
+++ SkillDemoTest1.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest1 {
+    @Test
+    public void subtraction() {
+        assertEquals(2, SkillDemo1.subtract(2,1));
+    }
+}
